e2392c7108d9139ae0fa33406a3d053510d79f3c
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2024-11-18 10:25:11 -0500

    Add files via upload

---
+++ root/src/CollectData.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 07:23:40 2024
+
+@author: Diego
+"""
+
+import os
+import pandas as pd
+
+class DataManager:
+    
+    def __init__(self) -> None:
+        
+        self.root_path      = os.path.abspath(
+            os.path.join((os.path.abspath(
+                os.path.join(os.getcwd(), os.pardir))), os.pardir))
+        
+        self.data_path      = os.path.join(self.root_path, "data")
+        self.raw_data_path  = os.path.join(self.data_path, "RawData")
+        
+        if os.path.exists(self.data_path) == False: os.makedirs(self.data_path)
+        if os.path.exists(self.raw_data_path) == False: os.makedirs(self.raw_data_path)
+        
+        self.bbg_xlsx_path = r"C:\Users\Diego\Desktop\app_prod\BBGData\root\BBGTickers.xlsx"
+        self.bbg_data_path = r"C:\Users\Diego\Desktop\app_prod\BBGData\data"
+        self.df_tickers    = (pd.read_excel(
+            io = self.bbg_xlsx_path, sheet_name = "tickers"))
+        
+        self.bbg_fut_path   = r"C:\Users\Diego\Desktop\app_prod\BBGFuturesManager\root\fut_tickers.xlsx"
+        self.bbg_front_path = r"C:\Users\Diego\Desktop\app_prod\BBGFuturesManager\data\PXFront"
+        self.bbg_deliv_path = r"C:\Users\Diego\Desktop\app_prod\BBGFuturesManager\data\BondDeliverableRisk"
+        
+        self.df_fut_tickers = (pd.read_excel(
+            io = self.bbg_fut_path, sheet_name = "px"))
+        
+        self.bad_breakevens = [
+            "USGGBE01 Index", "USGGBE09 Index", "USGGBE06 Index", 
+            "USGGBE08 Index", "USGGBE03 Index"]
+    
+    def get_inflation_swap(self, verbose: bool = False) -> pd.DataFrame: 
+        
+        file_path = os.path.join(self.raw_data_path, "InflationSwaps.parquet")
+        try:
+            
+            if verbose == True: print("Trying to find swap data")
+            df_out = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found Data\n")
+            
+        except: 
+        
+            if verbose == True: print("Couldn't find data, getting it")
+            df_tickers = (self.df_tickers.query(
+                "Subcategory == 'Interest Rate Swaps'").
+                assign(
+                    first  = lambda x: x.Description.str.split(" ").str[0],
+                    second = lambda x: x.Description.str.split(" ").str[1]).
+                query("first == 'USD' & second == 'Inflation'")
+                [["Security", "Description"]].
+                rename(columns = {"Security": "security"}))
+            
+            tickers = (df_tickers.assign(
+                tmp = lambda x: x.security.str.split(" ").str[0]).
+                tmp.
+                drop_duplicates().
+                sort_values().
+                to_list())
+            
+            files = [os.path.join(
+                self.bbg_data_path, ticker + ".parquet")
+                for ticker in tickers]
+            
+            df_out = (pd.read_parquet(
+                path = files, engine = "pyarrow").
+                drop(columns = ["variable"]).
+                merge(right = df_tickers, how = "inner", on = ["security"]))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = file_path, engine = "pyarrow")
+        
+        return df_out
+    
+    def get_breakeven(self, verbose: bool = False) -> pd.DataFrame: 
+        
+        file_path = os.path.join(self.raw_data_path, "BreakevenRates.parquet")
+        try:
+            
+            if verbose == True: print("Trying to find Breakeven data")
+            df_out = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found Data\n")
+        
+        except: 
+        
+            if verbose == True: print("Couldn't find data, getting it")
+            df_tickers = (self.df_tickers.query(
+                "Subcategory == 'Miscellaneous Indices'").
+                assign(
+                    first  = lambda x: x.Description.str.split(" ").str[0],
+                    second = lambda x: x.Description.str.split(" ").str[1]).
+                query("first == 'US' & second == 'Breakeven'")
+                [["Security", "Description"]].
+                rename(columns = {"Security": "security"}))
+            
+            tickers = (df_tickers.assign(
+                tmp = lambda x: x.security.str.split(" ").str[0]).
+                tmp.
+                drop_duplicates().
+                sort_values().
+                to_list())
+            
+            files = [os.path.join(
+                self.bbg_data_path, ticker + ".parquet")
+                for ticker in tickers]
+            
+            df_out = (pd.read_parquet(
+                path = files, engine = "pyarrow").
+                drop(columns = ["variable"]).
+                merge(right = df_tickers, how = "inner", on = ["security"]).
+                query("security != @self.bad_breakevens"))
+            
+            if verbose == True: print("Saving data\n")
+            df_out.to_parquet(path = file_path, engine = "pyarrow")
+        
+        return df_out
+    
+    def _get_tsy_rtn(self, df: pd.DataFrame) -> pd.DataFrame:
+        
+        df_out = (df.sort_values(
+            "date").
+            assign(
+                PX_rtn  = lambda x: x.PX_LAST.pct_change(),
+                PX_diff = lambda x: x.PX_LAST.diff(),
+                PX_bps  = lambda x: x.PX_diff / x.PX_dur).
+            dropna())
+        
+        return df_out
+    
+    def get_tsy_fut(self, verbose: bool = False) -> pd.DataFrame: 
+        
+        file_path = os.path.join(self.raw_data_path, "TreasuryFutures.parquet")
+        try:
+            
+            if verbose == True: print("Seaching for Treasury futures")
+            df_fut = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found Data\n")
+            
+        except: 
+            
+            if verbose == True: print("Couldn't find it, collecting")
+            raw_tickers = (self.df_fut_tickers.assign(
+                second = lambda x: x.name.str.split(" ").str[-2]).
+                query("second == 'Treasury'").
+                contract.
+                to_list())
+            
+            deliv_paths = ([
+                os.path.join(self.bbg_deliv_path, path_ + ".parquet") 
+                for path_ in raw_tickers])
+            
+            df_deliv = (pd.read_parquet(
+                path = deliv_paths, engine = "pyarrow").
+                pivot(
+                    index   = ["date", "security"], 
+                    columns = "variable",
+                    values  = "value").
+                reset_index().
+                rename(columns = {
+                    "CONVENTIONAL_CTD_FORWARD_FRSK": "PX_dur",
+                    "FUT_EQV_CNVX_NOTL"            : "PX_cnvx"}))
+            
+            fut_paths = ([
+                os.path.join(self.bbg_front_path, ticker + ".parquet") 
+                for ticker in raw_tickers])
+            
+            df_fut = (pd.read_parquet(
+                path = fut_paths, engine = "pyarrow").
+                merge(right = df_deliv, how = "inner", on = ["date", "security"]).
+                dropna().
+                groupby("security").
+                apply(self._get_tsy_rtn).
+                reset_index(drop = True))
+            
+            if verbose == True: print("Saving data\n")
+            df_fut.to_parquet(path = file_path, engine = "pyarrow")
+            
+        return df_fut
+        
+def main() -> None:
+        
+    DataManager().get_inflation_swap(verbose = True)
+    DataManager().get_breakeven(verbose = True)
+    DataManager().get_tsy_fut(verbose = True)
+    
+if __name__ == "__main__": main()
---
+++ root/src/DataPreprocess.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 07:31:23 2024
+
+@author: Diego
+"""
+
+import os
+import pandas as pd
+
+from CollectData import DataManager
+from sklearn.decomposition import PCA
+
+
+class InflationPCA(DataManager):
+    
+    def __init__(self) -> None: 
+
+        super().__init__()        
+        self.pca_path = os.path.join(self.data_path, "Signal")
+        if os.path.exists(self.pca_path) == False: os.makedirs(self.pca_path)
+        
+        self.n_comps = 3
+        
+    def _get_pca(self, df: pd.DataFrame) -> pd.DataFrame:
+      
+        df_wider = (df.drop(
+            columns = ["curve"]).
+            pivot(index = "date", columns = "security", values = "value").
+            dropna())
+        
+        df_out = (pd.DataFrame(
+            data    = PCA(self.n_comps).fit_transform(df_wider),
+            columns = ["PC{}".format(i + 1) for i in range(self.n_comps)],
+            index   = df_wider.index))
+        
+        return df_out
+    
+    def _lag_spread(self, df: pd.DataFrame) -> pd.DataFrame: 
+        
+        return(df.sort_values(
+            "date").
+            assign(lag_spread = lambda x: x.spread.shift()).
+            dropna())
+        
+    def get_pca(self, verbose: bool = False) -> pd.DataFrame: 
+        
+        file_path = os.path.join(self.pca_path, "PCASignal.parquet")
+        try:
+            
+            if verbose == True: print("Trying to find data")
+            df_combined = pd.read_parquet(path = file_path, engine = "pyarrow")
+            if verbose == True: print("Found Data\n")
+            
+        except: 
+        
+            if verbose == True: print("Couldn't find data, collecting it")
+            df_inflation = (self.get_inflation_swap().drop(
+                columns = ["Description"]).
+                assign(
+                    curve    = "inflation_swap",
+                    security = lambda x: x.security.str.split(" ").str[0]))
+            
+            df_breakeven = (self.get_breakeven().drop(
+                columns = ["Description"]).
+                assign(
+                    curve    = "tsy_breakeven",
+                    security = lambda x: x.security.str.split(" ").str[0]))
+            
+            df_combined = (pd.concat([
+                df_inflation, df_breakeven]).
+                groupby("curve").
+                apply(self._get_pca).
+                reset_index().
+                melt(id_vars = ["date", "curve"]).
+                pivot(index = ["date", "variable"], columns = "curve", values = "value").
+                dropna().
+                reset_index().
+                assign(spread = lambda x: x.inflation_swap - x.tsy_breakeven).
+                groupby("variable").
+                apply(self._lag_spread).
+                reset_index(drop = True))
+        
+            if verbose == True: print("Saving data\n")
+            df_combined.to_parquet(path = file_path, engine = "pyarrow")
+            
+        return df_combined
+
+if __name__ == "__main__": InflationPCA().get_pca(verbose = True)
